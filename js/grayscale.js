f5590966350c91d165c888333b0f43a775378a43
money2nepal Developer <developer@money2nepal.com.au>
2018-08-02 08:55:38 +1000

    Formatting saving total etc.

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,12 +1,14 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   var currencyAPI = "http://free.currencyconverterapi.com/api/v5/convert?q=AUD_NPR&compact=ultra";
   var rate;
   var ratePromotion = 1.5;
+  var total = 0;
 
+  //#region ThemeCode
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -20,7 +22,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -31,7 +33,7 @@
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
@@ -42,14 +44,15 @@
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
+  //#endregion
 
-  //$("#amountHelp").text("Today's exchange rate is Rs. 82.50 for one aussie dollar.");
+  //#region CurrencyConversion
   $.ajax({
     url: currencyAPI
-  }).done(function(data) {
-    if (data !== undefined && data != null){
+  }).done(function (data) {
+    if (data !== undefined && data != null) {
       var retrievedRate = data.AUD_NPR;
-      if (isNumber(retrievedRate)){
+      if (isNumber(retrievedRate)) {
         retrievedRate = retrievedRate + ratePromotion;
         rate = Math.round(retrievedRate * 100) / 100
         updateAmountText();
@@ -57,26 +60,21 @@
     }
   });
 
-  $("#amount").change(function(){
+  $("#amount").change(function () {
     updateAmountText();
   });
 
   function updateAmountText() {
-    if (isNumber(rate)){
-      var amount = $("#amount").val() ;
-      if(isNumber(amount)){
-        var total = Math.round(amount * rate);
+    if (isNumber(rate)) {
+      var amount = $("#amount").val();
+      if (isNumber(amount)) {
+        total = Math.round(amount * rate);
         $("#amountHelp").text("$" + numberWithCommas(amount) + " @ Rs." + rate + " will be Rs. " + nepaleseCurrencyCommas(total) + ".");
       } else {
         $("#amountHelp").text("Today's exchange rate is Rs. " + rate + " for one aussie dollar.");
       }
     }
   }
-
-  $("#transferForm").submit(function(event) {
-    event.preventDefault();
-    alert("Sorry, we are experience some issues today. Please try again tomorrow.")
-  });
 
   function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -90,20 +88,28 @@
     // 123 => ,123
     // 12,345 => ,12,345
     // 12,34,567 => ,12,34,567
-    for(var i = parts[0].length-1; i >= 0; i--){
-      if (numberWithCommas.length > 0 && (numberWithCommas.length % 3 == 0)){
+    for (var i = parts[0].length - 1; i >= 0; i--) {
+      if (numberWithCommas.length > 0 && (numberWithCommas.length % 3 == 0)) {
         numberWithCommas = "," + numberWithCommas;
       }
       // now add the digit
       numberWithCommas = parts[0][i] + numberWithCommas;
     }
     // add the part after decimal
-    if (parts.length > 1){
-      numberWithCommas = numberWithCommas+"."+parts[1];
+    if (parts.length > 1) {
+      numberWithCommas = numberWithCommas + "." + parts[1];
     }
     return numberWithCommas;
   }
 
-  function isNumber(n) { return !isNaN(parseFloat(n)) && !isNaN(n - 0) }
+  function isNumber(n) {
+    return !isNaN(parseFloat(n)) && !isNaN(n - 0)
+  }
+  //#endregion
+
+  $("#transferForm").submit(function (event) {
+    event.preventDefault();
+    alert("Sorry, we are experience some issues today. Please try again tomorrow.")
+  });
 
 })(jQuery); // End of use strict
