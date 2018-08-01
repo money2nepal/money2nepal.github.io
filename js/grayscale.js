627cb8404a3b58122e506f3c5c20627260e960d4
money2nepal Developer <developer@money2nepal.com.au>
2018-08-02 08:42:21 +1000

    Added currency conversion.

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,6 +1,10 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  var currencyAPI = "http://free.currencyconverterapi.com/api/v5/convert?q=AUD_NPR&compact=ultra";
+  var rate;
+  var ratePromotion = 1.5;
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -39,12 +43,67 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-  $("#amountHelp").text("Today's exchange rate is Rs. 82.50 for one aussie dollar.");
+  //$("#amountHelp").text("Today's exchange rate is Rs. 82.50 for one aussie dollar.");
+  $.ajax({
+    url: currencyAPI
+  }).done(function(data) {
+    if (data !== undefined && data != null){
+      var retrievedRate = data.AUD_NPR;
+      if (isNumber(retrievedRate)){
+        retrievedRate = retrievedRate + ratePromotion;
+        rate = Math.round(retrievedRate * 100) / 100
+        updateAmountText();
+      }
+    }
+  });
 
+  $("#amount").change(function(){
+    updateAmountText();
+  });
+
+  function updateAmountText() {
+    if (isNumber(rate)){
+      var amount = $("#amount").val() ;
+      if(isNumber(amount)){
+        var total = Math.round(amount * rate);
+        $("#amountHelp").text("$" + numberWithCommas(amount) + " @ Rs." + rate + " will be Rs. " + nepaleseCurrencyCommas(total) + ".");
+      } else {
+        $("#amountHelp").text("Today's exchange rate is Rs. " + rate + " for one aussie dollar.");
+      }
+    }
+  }
 
   $("#transferForm").submit(function(event) {
     event.preventDefault();
     alert("Sorry, we are experience some issues today. Please try again tomorrow.")
   });
+
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  function nepaleseCurrencyCommas(x) {
+    var numberWithCommas = "";
+    var parts = x.toString().split(".");
+    // To add commas, if we go backwards,
+    // we can simply add a comma when the length of the output is divisible by 3
+    // 123 => ,123
+    // 12,345 => ,12,345
+    // 12,34,567 => ,12,34,567
+    for(var i = parts[0].length-1; i >= 0; i--){
+      if (numberWithCommas.length > 0 && (numberWithCommas.length % 3 == 0)){
+        numberWithCommas = "," + numberWithCommas;
+      }
+      // now add the digit
+      numberWithCommas = parts[0][i] + numberWithCommas;
+    }
+    // add the part after decimal
+    if (parts.length > 1){
+      numberWithCommas = numberWithCommas+"."+parts[1];
+    }
+    return numberWithCommas;
+  }
+
+  function isNumber(n) { return !isNaN(parseFloat(n)) && !isNaN(n - 0) }
 
 })(jQuery); // End of use strict
